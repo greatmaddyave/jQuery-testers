73bf0723d0abdfc7ee0b071cf32f0083bc23c00d
John Cossack <Greatmadisonavenue@gmail.com>
2016-01-10 19:27:11 -0800

    new commit

---
+++ tester.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	var rubyText = "<h1>Ruby on Rails</h1>";
+	var nodeText = "<h1>Nodejs</h1>";
+	var javascriptText = "<h1>Javascript</h1>";
+	var jqueryText = "<h1>jQuery</h1>";
+	var htmlText = "<h1>HTML5</h1>";
+	var cssText = "<h1>CSS</h1>";
+	var bootstrapText = "<h1>Bootstrap</h1>";
+})
